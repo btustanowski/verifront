58a6efe27273ae0a3f12edb9bda66cfb83dad0d4
btustanowski <b.tustanowski@gmail.com>
2015-09-21 15:32:48 +0100

    hero unit complete

--- app/scripts/hero/hero.js
+++ app/scripts/hero/hero.js
@@ -12,7 +12,7 @@
         height = window.innerHeight;
         target = {x: width/2, y: height/2};
 
-        canvas = document.getElementById('demo-canvas');
+        canvas = document.getElementById('canvas');
         canvas.width = width;
         canvas.height = height;
         ctx = canvas.getContext('2d');
